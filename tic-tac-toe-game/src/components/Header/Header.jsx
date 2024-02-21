ee0a1813f6d01020838272a44dcb171daf19bb1d
agun36 <aguntoroti10@gmail.com>
2024-02-21 11:51:08 +0100

    I just modified /components/Header/Header.jsx

--- tic-tac-toe-game/src/components/Header/Header.jsx
+++ tic-tac-toe-game/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.scss";
+import gameLogo from '../../assets/game-logo.png';
 
 const Header = () => {
   return (
     <header>
-      <img src="game-logo.png" alt="Hand-drawn Tic-Tac-Toe game board" />
+      <img src={gameLogo} alt="Hand-drawn Tic-Tac-Toe game board" />
       <h1>React Tic-Tac-Toe</h1>
     </header>
   );
